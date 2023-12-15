a501372529b9ab23918d97c83317fdc7297bbff5
Bernat00 <szabobernat06@gmail.com>
2023-12-15 20:54:02 +0100

    ez má jó

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from app import create_app, create_socket
 
 
-app = create_app()
+app = create_app(server=False)
 socketio = create_socket(app)
 
 
 if __name__ == "__main__":
-    socketio.run(app)
-
-
+    socketio.run(app, debug=True)
 
--- app/__init__.py
+++ app/__init__.py
@@ -5,23 +5,26 @@ from app.chats import io_bp
 from app import security
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret!'
-    security.init_app(app)
+def create_app(server=True):
+	app = Flask(__name__)
+	app.config['SECRET_KEY'] = 'secret!'
+	security.init_app(app)
 
-    from app.user import bp as user_bp
-    app.register_blueprint(user_bp, url_prefix='/')
+	from app.user import bp as user_bp
+	app.register_blueprint(user_bp, url_prefix='/')
 
-    from app.chats import bp as chats_bp
-    app.register_blueprint(chats_bp, url_prefix='/chats')
+	from app.chats import bp as chats_bp
+	app.register_blueprint(chats_bp, url_prefix='/chats')
+	if server:
+		socketio = SocketIO(app)
+		io_bp.init_io(socketio)
 
-    return app
+	return app
 
 
 def create_socket(app):
-    socketio = SocketIO(app)
-    io_bp.init_io(socketio)
+	socketio = SocketIO(app)
+	io_bp.init_io(socketio)
 
-    return socketio
+	return socketio
 
